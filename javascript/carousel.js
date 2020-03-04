6cba0e84334ebfb1d300ab78dd2ba69465702acf
essarrgee <55785294+essarrgee@users.noreply.github.com>
2020-03-04 15:02:48 -0800

    Add IceTrial Game Page

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -5,13 +5,17 @@ var carouselViewButton = document.getElementById("carouselViewButton");
 
 function setCarouselBG(element) {
 	if (carouselViewButton && carouselBG && carouselBG2 && element) {
+		let screenshot = element.src;
 		carouselBG.classList.toggle("carousel-hidden");
 		carouselBG2.classList.toggle("carousel-hidden");
+		if (element.getAttribute("stored_img")) {
+			screenshot = element.getAttribute("stored_img");
+		}
 		if (carouselBG.classList.contains("carousel-hidden")) {
-			carouselBG2.src = element.src;
+			carouselBG2.src = screenshot;
 		}
 		else {
-			carouselBG.src = element.src;
+			carouselBG.src = screenshot;
 		}
 		carouselProjectTitle.innerHTML = element.id;
 		carouselViewButton.href = element.getAttribute("stored_href");
