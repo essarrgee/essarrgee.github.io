8cfb64b54c12d22932e58b3757c531dc65c8d16b
essarrgee <55785294+essarrgee@users.noreply.github.com>
2020-05-30 19:30:40 -0700

    Resolve TopNav Error
    -Remove use of "window.width()" in loadhtml.js
    -Respositioned "Applications" section and renamed to "Miscellaneous" in Projects page

--- javascript/loadhtml.js
+++ javascript/loadhtml.js
@@ -1,10 +1,4 @@
-var screenwidth = window.width();
-
 document.ready(function(){
 	$('#topNav').load("/sharedhtml/topNav.html");
 	$('#tictactoeGame').load("/sharedhtml/tictactoeGame.html");
 });
-
-if (screenwidth <= 600) {
-	$("#unityWebGL").remove();
-}
