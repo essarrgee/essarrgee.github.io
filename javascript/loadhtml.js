2af2364414b8ea3cf3f3a60426b95442fbedc88e
essarrgee <55785294+essarrgee@users.noreply.github.com>
2020-05-30 23:59:36 -0700

    Update loadhtml.js

--- javascript/loadhtml.js
+++ javascript/loadhtml.js
@@ -1,4 +1,7 @@
-document.ready(function(){
+$(document).ready(function(){
 	$('#topNav').load("/sharedhtml/topNav.html");
 	$('#tictactoeGame').load("/sharedhtml/tictactoeGame.html");
-})
+}) // Does not need a ";" here
+// Explanation: 
+// https://stackoverflow.com/questions/21755397/typeerror-jquery-ready-is-not-a-function
+
